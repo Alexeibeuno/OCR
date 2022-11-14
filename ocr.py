f086ded704a560b1893b47938c68c961027f02dc
Alexei Beuno H <67053046+Alexeibeuno@users.noreply.github.com>
2022-11-14 20:52:50 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -1,9 +1,5 @@
-try:
-    from PIL import Image
-except ImportError:
-    import Image
+from PIL import Image
 import pytesseract
-
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
